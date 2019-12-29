ef115a73cbdfa577166381bb64bc443d410e0b43
Rockyzsu <jinweizsu@gmail.com>
2019-12-29 16:02:00 +0800

    update

--- flask-video-streaming/camera.py
+++ flask-video-streaming/camera.py
@@ -1,16 +1,19 @@
 import time
 from base_camera import BaseCamera
 import cv2
+cascPath = 'haarcascade_frontalface_dataset.xml'  # dataset
+faceCascade = cv2.CascadeClassifier(cascPath)
 
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
     imgs = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
-    video = cv2.VideoCapture('mitmproxy.mp4')
+    # video = cv2.VideoCapture('mitmproxy.mp4')
+    video = cv2.VideoCapture(0)
     # 对于mp4的视频无效
-    video.set(cv2.CAP_PROP_FRAME_WIDTH,100)
-    video.set(cv2.CAP_PROP_FRAME_HEIGHT,100)
+    video.set(cv2.CAP_PROP_FRAME_WIDTH,300)
+    video.set(cv2.CAP_PROP_FRAME_HEIGHT,300)
 
     restart=False
 
@@ -25,19 +28,32 @@ class Camera(BaseCamera):
 
         print('been call')
         if Camera.restart:
-            Camera.video = cv2.VideoCapture('video_20191218_144737.mp4')
+            Camera.video = cv2.VideoCapture(0)
 
         while True:
 
             ret,frame = Camera.video.read()
             if ret:
-                _,img = cv2.imencode('.jpg',frame)
-                yield img.tobytes()
-
+                # frame=cv2.transpose(frame)
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             else:
                 time.sleep(1)
-                print('i am sleep')
-                # continue
-                Camera.restart=True
+                print('frame drop')
                 break
+            # 人脸识别
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            _,img = cv2.imencode('.jpg',frame)
+            yield img.tobytes()
+
 
--- live-stream-face-detection/camera.py
+++ live-stream-face-detection/camera.py
@@ -1,29 +1,32 @@
 import time
 import cv2
 
-cascPath = 'haarcascade_frontalface_dataset.xml'  # dataset
-faceCascade = cv2.CascadeClassifier(cascPath)
+
 
 #  for cctv camera'rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp'
 #  example of cctv or rtsp: 'rtsp://mamun:123456@101.134.16.117:554/user=mamun_password=123456_channel=1_stream=0.sdp'
 
 
-video_capture = cv2.VideoCapture('video_20191218_144737.mp4')  # 0 for web camera live stream
-
+# video_capture = cv2.VideoCapture('video_20191218_144737.mp4')  # 0 for web camera live stream
+cascPath = 'haarcascade_frontalface_dataset.xml'  # dataset
+faceCascade = cv2.CascadeClassifier(cascPath)
+video_capture = cv2.VideoCapture(0)  # 0 for web camera live stream
+print(video_capture.isOpened())
 
 def camera_stream():
 
     while True:
         # Capture frame-by-frame
-        ret, frame = video_capture.read()
-
-        if ret:
-            frame=cv2.transpose(frame)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        else:
-            time.sleep(1)
-            print('frame drop')
-            break
+        ret, frame = video_capture.read() # 为啥我的笔记本卡在这里？ A：不能放在全局
+        print(f'ret -- {ret}')
+        print('get frame')
+        # if ret:
+            # frame=cv2.transpose(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # else:
+        #     time.sleep(1)
+        #     print('frame drop')
+        #     break
         # 人脸识别
         faces = faceCascade.detectMultiScale(
             gray,
