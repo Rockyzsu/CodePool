bd2484269af7c0b2d8e13162176b5d3425c5a646
Rocky Chen <chenjinwei@beyebe.com>
2020-01-07 09:50:55 +0800

    添加注释

--- imagezmq-streaming/client.py
+++ imagezmq-streaming/client.py
@@ -8,16 +8,18 @@ import argparse
 import socket
 import time
 
+# 图像传送到树莓派
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True,
-	help="ip address of the server to which the client will connect")
+                help="ip address of the server to which the client will connect")
 args = vars(ap.parse_args())
 
 # initialize the ImageSender object with the socket address of the
 # server
 sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
-	args["server_ip"]))
+    args["server_ip"]))
 
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
@@ -25,8 +27,8 @@ rpiName = socket.gethostname()
 # vs = VideoStream(usePiCamera=True).start()
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
- 
+
 while True:
-	# read the frame from the camera and send it to the server
-	frame = vs.read()
-	sender.send_image(rpiName, frame)
+    # read the frame from the camera and send it to the server
+    frame = vs.read()
+    sender.send_image(rpiName, frame)
